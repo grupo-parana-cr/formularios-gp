db24a463fe31d74e7b3c046d87fbadd29cfd0232
grupoparana <isaque.barbosa@gparana.com.br>
2025-11-10 12:02:27 -0400

    Update script.js

--- solicitacao-contrato/script.js
+++ solicitacao-contrato/script.js
@@ -1302,7 +1302,7 @@
         formData.append(`documento_${index}`, file);
         });
         
-        const resp = await fetch('https://grupoparana-n8n.qkcade.easypanel.host/webhook/solicitacao-contrato', {
+        const resp = await fetch('https://grupoparana-n8n.qkcade.easypanel.host/webhook-test/solicitacao-contrato', {
           method: 'POST',
           body: formData
         });
@@ -1430,7 +1430,7 @@
       const search = $('#empresasSearch');
       const optionsDiv = $('#empresasOptions');
       const display = $('#empresasDisplay');
-      const selectedDiv = $('#window.selectedEmpresas');
+      const selectedDiv = $('#empresasSelected');
       const dataDiv = $('#empresasDataDisplay');
 
       // Preencher opções
@@ -1589,7 +1589,7 @@
       function updateDisplay() {
         const toggle = $('#empresasToggle');
         const display = $('#empresasDisplay');
-        const selectedDiv = $('#window.selectedEmpresas');
+        const selectedDiv = $('#empresasSelected');
         const dataDiv = $('#empresasDataDisplay');
         const errorDiv = $('#empresasError');
 
